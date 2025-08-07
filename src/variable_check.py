e77a43168c660638c885db3e8741ea630478f02b
Joshua Talib <ecm0847@ac6-100.bullx>
2025-08-07 13:35:28 +0000

    completed checks before downloading forecast

--- src/variable_check.py
+++
@@ -1,9 +0,0 @@
-# Check that requested variables are appropriate and are compatiable with WMO lead centre.
-
-def check_requested_variable(variable):
-    ''' In this function, check requested variable matches abbreviations used for the S2S database.
-    No return - will output ECDS function in variable_output.py
-    '''
-
-
-
